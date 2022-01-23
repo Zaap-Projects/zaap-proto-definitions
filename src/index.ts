14c5b1e00014d816e2dbbdabd59aeed566495b00
Christian Onwe <chrisoftech@gmail.com>
2022-01-23 19:54:00 +0000

    Reverted phone-number case.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // export public api from here
 // for example:
 // export * from './decorators';
-export * as phone_number_definitions from './proto/nestjs/src/proto/phone_number';
 export * as auth_definitions from './proto/nestjs/src/proto/auth';
 export * as user_definitions from './proto/nestjs/src/proto/user';
+
+export * as phone_number_definitions from './proto/nestjs/src/proto/phone_number';
